9afbd1af7684f8354972450b748deb6a816c2faf
muhammadqamar <muhammadqamar111@gmail.com>
2023-04-01 15:37:37 +0500

    redesign the app

--- src/C2EComponents/myc2e.js
+++ src/C2EComponents/myc2e.js
@@ -1,12 +1,9 @@
-import React, { useState, useRef } from "react";
-import JSZip from "jszip";
-import { Link } from "react-router-dom";
-import Myc2eOverview from "./myc2eoverview";
-import H5PEditor from "../H5PComponents/H5PEditors";
-import plusicon from "../assets/images/icons/plus-icon.png";
-import menuicon from "../assets/images/icons/home-menu-icon.svg";
-import searchmenu from "../assets/images/icons/search-menu-icon.svg";
-import ellipsisicon from "../assets/images/icons/ellipse-icon.svg";
+import React, { useState, useRef } from 'react';
+import c2e from '../assets/images/c2e.png';
+import logo from '../assets/images/logo.png';
+import upload from '../assets/images/upload (1).svg';
+import JSZip from 'jszip';
+import Myc2eOverview from './myc2eoverview';
 
 const Myc2e = () => {
   const [contentData, setcontentData] = useState();
@@ -16,128 +13,144 @@ const Myc2e = () => {
   const [activityh5p, setActivityh5p] = useState(null);
   const [loadzipper, setloadzipper] = useState(null);
   const inp = useRef();
-
   return (
-    <div className="main-wrapper reader-c2e">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-12"></div>
-          <div className="main-wrap">
-            <div className="main-title mb-4">
-              <h4>C2E Reader</h4>
-            </div>
-            <div className="my-c2e-cards-wrapper" id="offlineMyc2eContainer">
-              <div className="add-c2e-card" onClick={() => inp.current.click()}>
-                <div className="c2e-card-content" id="add-C2E">
-                  <img src={plusicon} width="20" height="20" />
-                  <p className="mt-1 add-c2e-text">
-                    <input
-                      ref={inp}
-                      type="file"
-                      style={{ display: "none" }}
-                      onChange={async (e) => {
-                        console.log("name", e.target.files);
-                        const loadzip = await JSZip.loadAsync(e.target.files[0]); //  read the zip file
-                        if (!loadzip.files["c2e.json"]) {
-                          alert("please uplaod only C2E Project, c2e.json not found.");
-                          return false;
-                        } else {
-                          // set state for all zip files
-                          setloadzipper(loadzip);
-                        }
+    <div className="reader-c2e">
+      <img src={logo} alt="logo" className="header-logo" />
+      {!contentDetail ? (
+        <div className="reader-main">
+          <div className="img-box">
+            <img src={c2e} alt="logo" />
+          </div>
+          <div className="uploadBox">
+            <div className="box">
+              <h1>Curriki Educational Experience Reader</h1>
+              <div
+                className="iconbox"
+                onClick={() => {
+                  inp.current.click();
+                }}
+              >
+                <img src={upload} alt="logo" />
+              </div>
+              <p className="text">Loreium ipsum dolet kilil asa</p>
+              <button
+                onClick={() => {
+                  inp.current.click();
+                }}
+              >
+                Choose File
+              </button>
+              <input
+                ref={inp}
+                type="file"
+                style={{ display: 'none' }}
+                onChange={async (e) => {
+                  console.log('name', e.target.files);
+                  const loadzip = await JSZip.loadAsync(e.target.files[0]); //  read the zip file
+                  if (!loadzip.files['c2e.json']) {
+                    alert(
+                      'please uplaod only C2E Project, c2e.json not found.'
+                    );
+                    return false;
+                  } else {
+                    // set state for all zip files
+                    setloadzipper(loadzip);
+                  }
 
-                        const contents = [];
-                        const contentsDetail = [];
-                        loadzip.forEach((relativePath, zipEntry) => {
-                          // load inner content for each file
-                          contents.push(zipEntry.name);
-                        });
-                        setcontentData(contents);
-                        // iterate each file through loop
-                        for (var i = 0; i < contents.length; i++) {
-                          const contentRead = await loadzip.files[contents[i]].async("text");
-                          contentsDetail.push(contentRead);
+                  const contents = [];
+                  const contentsDetail = [];
+                  loadzip.forEach((relativePath, zipEntry) => {
+                    // load inner content for each file
+                    contents.push(zipEntry.name);
+                  });
+                  setcontentData(contents);
+                  // iterate each file through loop
+                  for (var i = 0; i < contents.length; i++) {
+                    const contentRead = await loadzip.files[contents[i]].async(
+                      'text'
+                    );
+                    contentsDetail.push(contentRead);
 
+                    if (
+                      contents[i].includes('c2e.json') &&
+                      !contents[i].includes('activities')
+                    ) {
+                      const c2edata = JSON.parse(contentRead);
+                      c2edata?.c2eContain?.[0].c2eResources?.map(
+                        async (resource) => {
+                          // set project json after filter
+                          if (resource.url === 'project.json') {
+                            const contentProject = await loadzip.files[
+                              'project.json'
+                            ].async('text');
+                            setProjectJSON(JSON.parse(contentProject));
+                          }
+                          // extract array  of playlist depend on directory present in resoruce folder
                           if (
-                            contents[i].includes("c2e.json") &&
-                            !contents[i].includes("activities")
+                            resource.fileFormat === 'directory' &&
+                            resource.url !== 'activities' &&
+                            resource.url !== 'playlists'
                           ) {
-                            const c2edata = JSON.parse(contentRead);
-                            c2edata?.c2eContain?.[0].c2eResources?.map(async (resource) => {
-                              // set project json after filter
-                              if (resource.url === "project.json") {
-                                const contentProject = await loadzip.files["project.json"].async(
-                                  "text"
-                                );
-                                setProjectJSON(JSON.parse(contentProject));
-                              }
-                              // extract array  of playlist depend on directory present in resoruce folder
-                              if (
-                                resource.fileFormat === "directory" &&
-                                resource.url !== "activities" &&
-                                resource.url !== "playlists"
-                              ) {
-                                setplaylists((prevplaylists) => [...prevplaylists, resource.url]);
-                              }
-                            });
+                            setplaylists((prevplaylists) => [
+                              ...prevplaylists,
+                              resource.url,
+                            ]);
                           }
                         }
-                        setcontentDetail(contentsDetail);
-                      }}
-                    />
-                  </p>
-                </div>
-              </div>
-              {/* show project detail */}
-              {projectJSON && (
-                <div className="my-c2e-card">
-                  <div className="ellipses-dropdown">
-                    <img src={ellipsisicon} width="16px" height="16px" />
-                  </div>
-                  <div className="c2e-card-content">
-                    <Link
-                      onClick={() => {
-                        // setplaylists(projectJSON?.playlists);
-                      }}
-                    >
-                      <span className="project-heading">{projectJSON?.name}</span>
-                    </Link>
-                    <span>{projectJSON?.created_at}</span>
-                  </div>
-                </div>
-              )}
+                      );
+                    }
+                  }
+                  setcontentDetail(contentsDetail);
+                }}
+              />
             </div>
           </div>
-
-          <div className="col-12 remove-course-alert"></div>
         </div>
-      </div>
-      {/* playlist content component */}
-      <Myc2eOverview
-        playlistsContent={playlists}
-        setActivityh5p={setActivityh5p}
-        contentDetail={contentDetail}
-        contents={contentData}
-        loadzipper={loadzipper}
-      />
-      {/* h5p component and send h5p props for content*/}
-      <H5PEditor h5p={activityh5p} />
-      <div className="c23-bottom-menu">
-        <div className="menu-list">
-          <ul>
-            <li className="active-list">
-              <a href="#">
-                <img src={menuicon} width="24" height="24" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img src={searchmenu} width="24" height="24" />
-              </a>
-            </li>
-          </ul>
+      ) : (
+        <div className="playlist-informtion">
+          <button
+            onClick={() => {
+              setcontentDetail();
+            }}
+          >
+            Back
+          </button>
+          <Myc2eOverview
+            playlistsContent={playlists}
+            setActivityh5p={setActivityh5p}
+            contentDetail={contentDetail}
+            contents={contentData}
+            loadzipper={loadzipper}
+            activityh5p={activityh5p}
+          />
         </div>
-      </div>
+      )}
+      <footer class="footer-all">
+        <a
+          rel="noreferrer"
+          class="footer-link"
+          href="https://www.curriki.org/terms-of-service/"
+          target="_blank"
+        >
+          Terms of Use
+        </a>
+        <a
+          rel="noreferrer"
+          class="footer-link"
+          href="https://www.curriki.org/privacy-policy/"
+          target="_blank"
+        >
+          Privacy Policy
+        </a>
+        <a
+          rel="noreferrer"
+          class="footer-link"
+          href="https://www.currikistudio.org/help/"
+          target="_blank"
+        >
+          Help Center
+        </a>
+      </footer>
     </div>
   );
 };
--- src/C2EComponents/myc2eoverview.js
+++ src/C2EComponents/myc2eoverview.js
@@ -1,10 +1,8 @@
-import React, { useEffect, useState } from "react";
-import Tab from "react-bootstrap/Tab";
-import Tabs from "react-bootstrap/Tabs";
-import Accordion from "react-bootstrap/Accordion";
-import courselist from "../assets/images/course-list-img.png";
-import homemenuicon from "../assets/images/icons/home-menu-icon.svg";
-import searchmenuicon from "../assets/images/icons/search-menu-icon.svg";
+import React, { useEffect, useState, useRef } from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+import courselist from '../assets/images/course-list-img.png';
+import activityfile from '../assets/images/main-banner-img.png';
+import H5PEditor from '../H5PComponents/H5PEditors';
 
 const Myc2eOverview = ({
   playlistsContent,
@@ -12,141 +10,107 @@ const Myc2eOverview = ({
   contentDetail,
   contents,
   loadzipper,
+  activityh5p,
 }) => {
   const [totalActivities, settotalActivities] = useState([]);
   const returnIndex = (filename) => {
-    return contents.indexOf(contents.filter((data, counter) => data.includes(filename))?.[0]);
+    return contents.indexOf(
+      contents.filter((data, counter) => data.includes(filename))?.[0]
+    );
   };
+  const hide = useRef()
   useEffect(() => {
     (async () => {
       // iterate again through each file in zip directory
       for (var i = 0; i < contents.length; i++) {
-        const contentRead = await loadzipper.files[contents[i]].async("text");
+        const contentRead = await loadzipper.files[contents[i]].async('text');
         // filter total number for activities based on c2e json in activity folder
-        if (contents[i].includes("/activities/c2e.json")) {
+        if (contents[i].includes('/activities/c2e.json')) {
           const c2edata = JSON.parse(contentRead);
           c2edata?.c2eContain?.[1].c2eComponents?.map(async (resource) => {
-            settotalActivities((prevtotalActivities) => [...prevtotalActivities, resource]);
+            settotalActivities((prevtotalActivities) => [
+              ...prevtotalActivities,
+              resource,
+            ]);
           });
         }
       }
     })();
   }, [playlistsContent]);
-  console.log("contentDetail", contentDetail);
-  return (
-    <div className="main-wrapper">
-      {playlistsContent?.length > 0 && (
-        <>
-          <div className="container-fluid">
-            <div className="row">
-              <div className="main-wrap">
-                <div className="course-detail-wrap">
-                  <div className="thumb-content">
-                    <p id="course-title"></p>
-                    <img src={courselist} id="course-image" />
-                  </div>
-                  <div className="tab-content">
-                    <Tabs
-                      defaultActiveKey="Overview"
-                      id="uncontrolled-tab-example"
-                      classNameName="mb-3"
-                    >
-                      <Tab className="nav-link" eventKey="Overview" title="Overview">
-                        <div className="tab-content" id="myTabContent">
-                          <div
-                            className="tab-pane fade show active"
-                            id="overview"
-                            role="tabpanel"
-                            aria-labelledby="overview-tab"
-                          >
-                            <div className="overview-description">
-                              <h4>Description</h4>
-                              <p id="course-description">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum
-                                est suspendisse tempor dui. Fringilla dis tincidunt morbi risus
-                                interdum urna, odio ac. Sit in venenatis gravida enim vitae, nibh
-                                blandit molestie. Et, nulla nisl laoreet vel tincidunt enim,
-                                venenatis. Viverra eget lobortis massa viverra at faucibus mauris
-                                suspendisse. Elit dis.
-                              </p>
-                            </div>
-                            <div className="overview-description">
-                              <h4>Marketplace:</h4>
-                              <p>Bill’s Marketplace</p>
-                            </div>
-                            <div className="overview-description">
-                              <h4>Author:</h4>
-                              <p id="author-name">Jenny Wilson</p>
-                            </div>
-                          </div>
-                        </div>
-                      </Tab>
 
-                      <Tab eventKey="Content" title="Content">
-                        <div id="main">
-                          <h4 className="playlist-head">Playlists</h4>
-                        </div>
-                        <Accordion defaultActiveKey="0">
-                          {playlistsContent?.map((playlist, key) => {
-                            return (
-                              <Accordion.Item eventKey={key}>
-                                <Accordion.Header>{playlist}</Accordion.Header>
-                                {totalActivities?.map((activity) => {
-                                  if (activity?.subManifest?.includes(playlist)) {
-                                    return (
-                                      <Accordion.Body
-                                        onClick={async () => {
-                                          // on activity click find your respective activity from the total activity
-                                          for (var i = 0; i < contents.length; i++) {
-                                            const contentRead = await loadzipper.files[
-                                              contents[i]
-                                            ].async("text");
-                                            // contentsDetail.push(contentRead);
-                                            if (
-                                              contents[i].includes(activity.name) &&
-                                              contents[i].includes("-h5p")
-                                            ) {
-                                              // set h5p data for repsective activity
-                                              setActivityh5p(JSON.parse(contentRead));
-                                            }
-                                          }
-                                        }}
-                                      >
-                                        {activity.name}
-                                      </Accordion.Body>
-                                    );
-                                  } else {
-                                    <h3>No Activity found</h3>;
-                                  }
-                                })}
-                              </Accordion.Item>
-                            );
-                          })}
-                        </Accordion>
-                      </Tab>
-                    </Tabs>
-                  </div>
-                </div>
-              </div>
+  return (
+    <div>
+      {playlistsContent?.length > 0 && (
+        <div className="second-view" >
+          <div className="info" ref={hide}>
+            <img src={courselist} alt="" id="course-image" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum
+              est suspendisse tempor dui. Fringilla dis tincidunt morbi risus
+              interdum urna, odio ac. Sit in venenatis gravida enim vitae, nibh
+              blandit molestie. Et, nulla nisl laoreet vel tincidunt enim,
+              venenatis. Viverra eget lobortis massa viverra at faucibus mauris
+              suspendisse. Elit dis.
+            </p>
+            <div className="overview-description">
+              <h4>Marketplace:</h4>
+              <p>Bill’s Marketplace</p>
+            </div>
+            <div className="overview-description">
+              <h4>Author:</h4>
+              <p id="author-name">Jenny Wilson</p>
             </div>
           </div>
-          <div className="c23-bottom-menu">
-            <div className="menu-list">
-              <ul>
-                <li className="active-list">
-                  <a href="#">
-                    <img src={homemenuicon} width="24" height="24" />
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <img src={searchmenuicon} width="24" height="24" />
-                  </a>
-                </li>
-              </ul>
-            </div>
+          <div className="accordian-c2e">
+            <Accordion defaultActiveKey="0">
+              {playlistsContent?.map((playlist, key) => {
+                return (
+                  <Accordion.Item eventKey={String(key)}>
+                    <Accordion.Header>{playlist}</Accordion.Header>
+                    {totalActivities?.map((activity) => {
+                      if (activity?.subManifest?.includes(playlist)) {
+                        return (
+                          <Accordion.Body
+                            onClick={async () => {
+                              // on activity click find your respective activity from the total activity
+                              for (var i = 0; i < contents.length; i++) {
+                                const contentRead = await loadzipper.files[
+                                  contents[i]
+                                ].async('text');
+                                // contentsDetail.push(contentRead);
+                                if (
+                                  contents[i].includes(activity.name) &&
+                                  contents[i].includes('-h5p')
+                                ) {
+                                  // set h5p data for repsective activity
+                                  setActivityh5p(JSON.parse(contentRead));
+                                }
+                              }
+                            }}
+                          >
+                            <div className="activity-detail" onClick={()=>{
+                              hide.current.style.display='none'
+                            }}>
+                              <img src={activityfile} alt="" />
+                              {activity.name}
+                            </div>
+                          </Accordion.Body>
+                        );
+                      } else {
+                        <h3>No Activity found</h3>;
+                      }
+                    })}
+                  </Accordion.Item>
+                );
+              })}
+            </Accordion>
           </div>
-        </>
+          {activityh5p &&
+          <div style={{flex:15}}>
+          <H5PEditor h5p={activityh5p} />
+          </div>
+}
+        </div>
       )}
     </div>
   );
