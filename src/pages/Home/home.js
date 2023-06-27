7093210fa6dd09c6d0bb99efdf9bba97764f1931
muhammadqamar <muhammadqamar111@gmail.com>
2023-06-27 16:27:52 +0500

    more changes

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -4,15 +4,15 @@ import addIcon from '../../assets/images/icons/add-icon.svg';
 import bgImg from '../../assets/images/principles-micro.png';
 import NavigationIcon from '../../assets/images/icons/navigation-icon.svg';
 import DownloadIcon from '../../assets/images/icons/actions-download.svg';
-
-const Home = ({walletConnection}) => {
+import { projectdata } from '../../C2EComponents/data';
+const Home = ({ walletConnection }) => {
   return (
     <div className="main-container">
       <div className="heading">
         <h3>
           <span>Hello,</span>
           <br />
-           {walletConnection?.name}
+          {walletConnection?.name}
         </h3>
       </div>
       <div className="search-wrapper">
@@ -32,27 +32,31 @@ const Home = ({walletConnection}) => {
           <img src={addIcon} alt="add icon" />
           <h5 className="add-text">Add C2E</h5>
         </div>
-        <div
-          className="add-img-card "
-          style={{
-            backgroundImage: `url(${bgImg})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
-          }}
-        >
-          <div className="dropdown-icon">
-            <img src={NavigationIcon} alt="navigation" />
-          </div>
-          {/* <div className="add-more-img">
-            <p className="">Add</p>
-            <img src={DownloadIcon} alt="download" />
-          </div> */}
-          <div className="card-detail">
-            <h5 className="card-text">Add C2E</h5>
-            <p className="">13 Jan 2023</p>
-          </div>
-        </div>
+        {projectdata?.map((data) => {
+          return (
+            <div
+              className="add-img-card "
+              style={{
+                backgroundImage: `url(${data?.general?.thumb_url})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+              }}
+            >
+              <div className="dropdown-icon">
+                <img src={NavigationIcon} alt="navigation" />
+              </div>
+              {/* <div className="add-more-img">
+              <p className="">Add</p>
+              <img src={DownloadIcon} alt="download" />
+            </div> */}
+              <div className="card-detail">
+                <h5 className="card-text">{data?.general?.title}</h5>
+                {/* <p className="">13 Jan 2023</p> */}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
