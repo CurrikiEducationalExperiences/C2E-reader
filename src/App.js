6a7139b6ef6e2d0889f80fcf3a065f41e8427e48
muhammadqamar <muhammadqamar111@gmail.com>
2023-06-27 20:05:12 +0500

    login screen design

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Myc2e from './C2EComponents/myc2e';
 import Home from './pages/Home/home';
 import Overview from './pages/Overview/overview';
+import Login from './pages/login';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -18,6 +19,9 @@ function App() {
         </Route>
         <Route exact path="/overview">
           <Overview />
+        </Route>
+        <Route exact path="/login">
+          <Login />
         </Route>
       </Switch>
     </Router>
