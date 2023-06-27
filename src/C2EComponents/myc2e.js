8f334ff6282e2c8c58830a57c48b2d373a0d59f3
muhammadqamar <muhammadqamar111@gmail.com>
2023-06-27 15:43:16 +0500

    more changes

--- src/C2EComponents/myc2e.js
+++ src/C2EComponents/myc2e.js
@@ -8,11 +8,13 @@ import 'react-circular-progressbar/dist/styles.css';
 import { OpenloginAdapter } from '@web3auth/openlogin-adapter';
 import { Web3Auth } from '@web3auth/modal';
 import { ADAPTER_EVENTS } from '@web3auth/base';
+import Spinner from 'react-bootstrap/Spinner';
+
 import JSZip from 'jszip';
 import Modal from 'react-bootstrap/Modal';
 // import Myc2eOverview from './myc2eoverview';
 import Header from './header';
-
+import { projectdata } from './data';
 import upload from '../assets/images/upload (1).svg';
 
 const Myc2e = () => {
@@ -25,6 +27,7 @@ const Myc2e = () => {
   const [walletConnection, setWalletConneciton] = useState(null);
   const [uploadProgress, setUploadProgress] = useState(0);
   const [modalShow, setModalShow] = React.useState(false);
+  const [modalShow1, setModalShow1] = React.useState(false);
 
   const [c2e, c2edetail] = useState(null);
   const [projects, setProjects] = useState(null);
@@ -101,6 +104,12 @@ const Myc2e = () => {
     })();
   }, [allFiles]);
 
+  useEffect(() => {
+    if (projects?.length) {
+      setModalShow1(true);
+    }
+  }, [projects]);
+
   const returnContentFromUrl = async (url) => {
     for (var i = 0; i < allFiles.length; i++) {
       const contentRead = await JSlipParser.files[allFiles[i]].async('text');
@@ -167,121 +176,135 @@ const Myc2e = () => {
     <div className="reader-c2e">
       <Header web3auth={web3auth} walletConnection={walletConnection} />
       {!projects?.length ? (
-        <div className="reader-main">
-          {/*<div className="img-box">
-            <img src={c2e} alt="logo" />
-           </div>*/}
-
-          {walletConnection ? (
-            <div className="login-text text-detail">
-              <h3>How does it work?</h3>
-              <p>
-                After you have licensed a C2E from a digital marketplace, you
-                will receive an email with instructions on how to download it.
-                <br /> After the C2E is in your hand, you can open it in a C2E
-                reader, like this one.
-              </p>
-            </div>
-          ) : (
-            <div className="text-detail">
-              <h5>Imagine a world…</h5>
-              <p>
-                … where access to high quality learning resources is equitable,
-                affordable, and widely available. <br />
-                <br /> … where digital content creators have access to resources
-                and incentives to build quality interactive learning
-                experiences, and marketplaces where they can get paid fairly for
-                their efforts.
-                <br /> <br /> … where the quality and appropriateness of digital
-                educational content can be vetted before it enters a
-                marketplace.
-              </p>
-            </div>
-          )}
-
-          <div className="uploadBox">
-            <div className="box">
-              <h1>Curriki Educational Experiences Reader</h1>
-              {walletConnection && (
-                <div className="iconbox">
-                  <CircularProgressbarWithChildren value={uploadProgress}>
-                    <img src={upload} alt="" />
-                  </CircularProgressbarWithChildren>
+        <>
+          <h1 className="myC2E">My C2E's</h1>
+          <div className="myproject">
+            {projectdata.map((data) => {
+              return (
+                <div className="block">
+                  <div
+                    className="bg"
+                    style={{
+                      backgroundImage: `url(${data?.general?.thumb_url})`,
+                    }}
+                  />
+                  <h4>{data?.general?.title}</h4>
                 </div>
-              )}
-              {walletConnection ? (
-                <p className="text">Upload C2E from your local device</p>
-              ) : (
-                <p className="text text-space">
-                  Log In and Experience C2Es NOW
+              );
+            })}
+          </div>
+          <div className="reader-main">
+            {walletConnection ? (
+              <div className="login-text text-detail">
+                <h3>How does it work?</h3>
+                <p>
+                  After you have licensed a C2E from a digital marketplace, you
+                  will receive an email with instructions on how to download it.
+                  <br /> After the C2E is in your hand, you can open it in a C2E
+                  reader, like this one.
                 </p>
-              )}
-              {walletConnection ? (
-                <button
-                  onClick={() => {
-                    inp.current.click();
+              </div>
+            ) : (
+              <div className="text-detail">
+                <h5>Imagine a world…</h5>
+                <p>
+                  … where access to high quality learning resources is
+                  equitable, affordable, and widely available. <br />
+                  <br /> … where digital content creators have access to
+                  resources and incentives to build quality interactive learning
+                  experiences, and marketplaces where they can get paid fairly
+                  for their efforts.
+                  <br /> <br /> … where the quality and appropriateness of
+                  digital educational content can be vetted before it enters a
+                  marketplace.
+                </p>
+              </div>
+            )}
+
+            <div className="uploadBox">
+              <div className="box">
+                <h1>Curriki Educational Experiences Reader</h1>
+                {walletConnection && (
+                  <div className="iconbox">
+                    <CircularProgressbarWithChildren value={uploadProgress}>
+                      <img src={upload} alt="" />
+                    </CircularProgressbarWithChildren>
+                  </div>
+                )}
+                {walletConnection ? (
+                  <p className="text">Upload C2E from your local device</p>
+                ) : (
+                  <p className="text text-space">
+                    Log In and Experience C2Es NOW
+                  </p>
+                )}
+                {walletConnection ? (
+                  <button
+                    onClick={() => {
+                      inp.current.click();
+                    }}
+                  >
+                    Choose File
+                  </button>
+                ) : (
+                  <button onClick={() => login()}>LET’s GET STARTED!</button>
+                )}
+                <input
+                  ref={inp}
+                  type="file"
+                  style={{ display: 'none' }}
+                  onChange={async (e) => {
+                    const loadzip = await JSZip.loadAsync(e.target.files[0]);
+                    setJSlipParser(loadzip);
+                    // console.log('name', e.target.files);
+                    // const loadzip = await JSZip.loadAsync(e.target.files[0]); //  read the zip file
+                    // if (!loadzip.files['c2e.json']) {
+                    //   alert('please uplaod only C2E Project, c2e.json not found.');
+                    //   return false;
+                    // } else {
+                    //   // set state for all zip files
+                    //   setloadzipper(loadzip);
+                    // }
+
+                    // const contents = [];
+                    // const contentsDetail = [];
+                    // loadzip.forEach((relativePath, zipEntry) => {
+                    //   // load inner content for each file
+                    //   contents.push(zipEntry.name);
+                    // });
+                    // setcontentData(contents);
+                    // // iterate each file through loop
+                    // for (var i = 0; i < contents.length; i++) {
+                    //   const contentRead = await loadzip.files[contents[i]].async('text');
+                    //   contentsDetail.push(contentRead);
+
+                    //   if (contents[i].includes('c2e.json') && !contents[i].includes('activities')) {
+                    //     const c2edata = JSON.parse(contentRead);
+                    //     c2edata?.c2eContain?.[0].c2eResources?.map(async (resource) => {
+                    //       // set project json after filter
+                    //       if (resource.url === 'project.json') {
+                    //         const contentProject = await loadzip.files['project.json'].async('text');
+                    //         setProjectJSON(JSON.parse(contentProject));
+                    //       }
+
+                    //       // extract array  of playlist depend on directory present in resoruce folder
+                    //       if (
+                    //         resource.fileFormat === 'directory' &&
+                    //         resource.url !== 'activities' &&
+                    //         resource.url !== 'playlists'
+                    //       ) {
+                    //         setplaylists((prevplaylists) => [...prevplaylists, resource.url]);
+                    //       }
+                    //     });
+                    //   }
+                    // }
+                    // setcontentDetail(contentsDetail);
                   }}
-                >
-                  Choose File
-                </button>
-              ) : (
-                <button onClick={() => login()}>LET’s GET STARTED!</button>
-              )}
-              <input
-                ref={inp}
-                type="file"
-                style={{ display: 'none' }}
-                onChange={async (e) => {
-                  const loadzip = await JSZip.loadAsync(e.target.files[0]);
-                  setJSlipParser(loadzip);
-                  // console.log('name', e.target.files);
-                  // const loadzip = await JSZip.loadAsync(e.target.files[0]); //  read the zip file
-                  // if (!loadzip.files['c2e.json']) {
-                  //   alert('please uplaod only C2E Project, c2e.json not found.');
-                  //   return false;
-                  // } else {
-                  //   // set state for all zip files
-                  //   setloadzipper(loadzip);
-                  // }
-
-                  // const contents = [];
-                  // const contentsDetail = [];
-                  // loadzip.forEach((relativePath, zipEntry) => {
-                  //   // load inner content for each file
-                  //   contents.push(zipEntry.name);
-                  // });
-                  // setcontentData(contents);
-                  // // iterate each file through loop
-                  // for (var i = 0; i < contents.length; i++) {
-                  //   const contentRead = await loadzip.files[contents[i]].async('text');
-                  //   contentsDetail.push(contentRead);
-
-                  //   if (contents[i].includes('c2e.json') && !contents[i].includes('activities')) {
-                  //     const c2edata = JSON.parse(contentRead);
-                  //     c2edata?.c2eContain?.[0].c2eResources?.map(async (resource) => {
-                  //       // set project json after filter
-                  //       if (resource.url === 'project.json') {
-                  //         const contentProject = await loadzip.files['project.json'].async('text');
-                  //         setProjectJSON(JSON.parse(contentProject));
-                  //       }
-
-                  //       // extract array  of playlist depend on directory present in resoruce folder
-                  //       if (
-                  //         resource.fileFormat === 'directory' &&
-                  //         resource.url !== 'activities' &&
-                  //         resource.url !== 'playlists'
-                  //       ) {
-                  //         setplaylists((prevplaylists) => [...prevplaylists, resource.url]);
-                  //       }
-                  //     });
-                  //   }
-                  // }
-                  // setcontentDetail(contentsDetail);
-                }}
-              />
+                />
+              </div>
             </div>
           </div>
-        </div>
+        </>
       ) : (
         <div className="playlist-informtion">
           {/* <button
@@ -334,10 +357,13 @@ const Myc2e = () => {
                         {activity?.map((activity) => {
                           return (
                             <Accordion.Body>
-                              <div className="activity-detail" onClick={()=>{
-                                 setActivityh5p(activity)
-                                setModalShow(true)
-                              }}>
+                              <div
+                                className="activity-detail"
+                                onClick={() => {
+                                  setActivityh5p(activity);
+                                  setModalShow(true);
+                                }}
+                              >
                                 <img src={activityfile} alt="" />
                                 {activity?.title}
                               </div>
@@ -355,6 +381,10 @@ const Myc2e = () => {
                 onHide={() => setModalShow(false)}
                 activityh5p={activityh5p}
               />
+              <MyVerticallyCenteredModal1
+                show={modalShow1}
+                onHide={() => setModalShow1(false)}
+              />
             </div>
           </div>
         </div>
@@ -364,8 +394,8 @@ const Myc2e = () => {
 };
 
 function MyVerticallyCenteredModal(props) {
-  const {activityh5p} = props
-  console.log(activityh5p)
+  const { activityh5p } = props;
+  console.log(activityh5p);
   return (
     <Modal
       {...props}
@@ -374,16 +404,41 @@ function MyVerticallyCenteredModal(props) {
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          C2E Player
-        </Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">C2E Player</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-      {!!activityh5p && (
-                <div style={{ flex: 15 }}>
-                  <H5PEditor h5p={activityh5p.h5pSettingsJson?.h5p} />
-                </div>
-              )}
+        {!!activityh5p && (
+          <div style={{ flex: 15 }}>
+            <H5PEditor h5p={activityh5p.h5pSettingsJson?.h5p} />
+          </div>
+        )}
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+function MyVerticallyCenteredModal1(props) {
+  const { activityh5p } = props;
+  console.log(activityh5p);
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter"></Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="validator">
+          <div>
+            <Spinner variant="primary" animation="grow" />
+            <Spinner variant="primary" animation="grow" />
+            <Spinner variant="primary" animation="grow" />
+          </div>
+          We are validing C2E licensing information and description.
+        </div>
       </Modal.Body>
     </Modal>
   );
