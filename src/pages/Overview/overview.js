20a34bdaba32d25d561fbfbfc08a935c0fc9ac37
muhammadqamar <muhammadqamar111@gmail.com>
2023-06-27 16:07:33 +0500

    added new pages

--- src/pages/Overview/overview.js
+++ src/pages/Overview/overview.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Accordion } from 'react-bootstrap';
+import { Accordion, Tab, Tabs } from 'react-bootstrap';
+
 import BannerImg from '../../assets/images/s2e-banner.png';
 const Overview = () => {
   return (
@@ -8,61 +9,70 @@ const Overview = () => {
       <div className="banner-img">
         <img src={BannerImg} alt="banner" />
       </div>
-      <div className="flex-box">
-        <button className="filter-box active btn-width">Overview</button>
-        <button className="filter-box btn-width">Content</button>
-      </div>
-      <div className="c2e-details">
-        <div>
-          <h5>Description</h5>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum
-            est suspendisse tempor dui. Fringilla dis tincidunt morbi risus
-            interdum urna, odio ac. Sit in venenatis gravida enim vitae, nibh
-            blandit molestie. Et, nulla nisl laoreet vel tincidunt enim,
-            venenatis. Viverra eget lobortis massa viverra at faucibus mauris
-            suspendisse. Elit dis.
-          </p>
-        </div>
-        <div>
-          <h5>Marketplace:</h5>
-          <p>Bill’s Marketplace</p>
-        </div>
-        <div>
-          <h5>Author:</h5>
-          <p>Jenny Wilson</p>
-        </div>
-      </div>
-      <h2 className="playlist-heading">Playlists</h2>
-      <div className="playlist-accordion">
-        <Accordion defaultActiveKey="0">
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>Playlist #1</Accordion.Header>
-            <Accordion.Body>
-              <div className="playlist-informtion-box">
-                <div className="plylist-activity">
-                  <div className="p-a-img">
-                    <img src={BannerImg} alt="" />
-                  </div>
-                  <p>Activity #1</p>
-                </div>
-                <div className="plylist-activity">
-                  <div className="p-a-img">
-                    <img src={BannerImg} alt="" />
-                  </div>
-                  <p>Activity #1</p>
-                </div>
 
-                <div className="plylist-activity">
-                  <div className="p-a-img">
-                    <img src={BannerImg} alt="" />
-                  </div>
-                  <p>Activity #1</p>
-                </div>
+      <div className="tabs-wrapper">
+        <Tabs
+          defaultActiveKey="Overview"
+          id="uncontrolled-tab-example"
+          className="mb-3"
+        >
+          <Tab eventKey=" Overview" title=" Overview">
+            <div className="c2e-details">
+              <div>
+                <h5>Description</h5>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Bibendum est suspendisse tempor dui. Fringilla dis tincidunt
+                  morbi risus interdum urna, odio ac. Sit in venenatis gravida
+                  enim vitae, nibh blandit molestie. Et, nulla nisl laoreet vel
+                  tincidunt enim, venenatis. Viverra eget lobortis massa viverra
+                  at faucibus mauris suspendisse. Elit dis.
+                </p>
               </div>
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
+              <div>
+                <h5>Marketplace:</h5>
+                <p>Bill’s Marketplace</p>
+              </div>
+              <div>
+                <h5>Author:</h5>
+                <p>Jenny Wilson</p>
+              </div>
+            </div>
+          </Tab>
+          <Tab eventKey="profile" title="Content">
+            <h2 className="playlist-heading">Playlists</h2>
+            <div className="playlist-accordion">
+              <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>Playlist #1</Accordion.Header>
+                  <Accordion.Body>
+                    <div className="playlist-informtion-box">
+                      <div className="plylist-activity">
+                        <div className="p-a-img">
+                          <img src={BannerImg} alt="" />
+                        </div>
+                        <p>Activity #1</p>
+                      </div>
+                      <div className="plylist-activity">
+                        <div className="p-a-img">
+                          <img src={BannerImg} alt="" />
+                        </div>
+                        <p>Activity #1</p>
+                      </div>
+
+                      <div className="plylist-activity">
+                        <div className="p-a-img">
+                          <img src={BannerImg} alt="" />
+                        </div>
+                        <p>Activity #1</p>
+                      </div>
+                    </div>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </div>
+          </Tab>
+        </Tabs>
       </div>
     </div>
   );
