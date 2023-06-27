734fcadc822eedb9a3a945a23e0af9ff1578d3ad
muhammadqamar <muhammadqamar111@gmail.com>
2023-06-27 15:40:58 +0500

    added new pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Myc2e from './C2EComponents/myc2e';
+import Home from './pages/Home/home';
+import Overview from './pages/Overview/overview';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -9,6 +12,12 @@ function App() {
       <Switch>
         <Route exact path="/">
           <Myc2e />
+        </Route>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/overview">
+          <Overview />
         </Route>
       </Switch>
     </Router>
---
+++ src/pages/Home/home.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import searchIcon from '../../assets/images/icons/search1.png';
+import addIcon from '../../assets/images/icons/add-icon.svg';
+import bgImg from '../../assets/images/principles-micro.png';
+import NavigationIcon from '../../assets/images/icons/navigation-icon.svg';
+import DownloadIcon from '../../assets/images/icons/actions-download.svg';
+
+const Home = () => {
+  return (
+    <div className="main-container">
+      <div className="heading">
+        <h3>
+          <span>Hello,</span>
+          <br />
+          Jane Coooper
+        </h3>
+      </div>
+      <div className="search-wrapper">
+        <input type="text" placeholder="Search your c2e..." />
+        <div className="search-icon">
+          <img src={searchIcon} alt="search" />
+        </div>
+      </div>
+      <h4 className="sub-heading">My C2E’s</h4>
+      <div className="sort-filter">
+        <span className="filter-box active">All</span>
+        <span className="filter-box">Due date</span>
+      </div>
+      <div className="c2e-cards">
+        <div className="add-card">
+          <img src={addIcon} alt="add icon" />
+          <h5 className="add-text">Add C2E</h5>
+        </div>
+        <div
+          className="add-img-card "
+          style={{
+            backgroundImage: `url(${bgImg})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+          }}
+        >
+          {/* <div className="dropdown-icon">
+            <img src={NavigationIcon} alt="navigation" />
+          </div> */}
+          <div className="add-more-img">
+            <p className="">Add</p>
+            <img src={DownloadIcon} alt="download" />
+          </div>
+          <div className="card-detail">
+            <h5 className="card-text">Add C2E</h5>
+            <p className="">13 Jan 2023</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
